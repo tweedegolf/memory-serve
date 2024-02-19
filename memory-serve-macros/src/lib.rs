22a28ac904c603c65e048dbb483fe7cd5cd8f0ab
Benjamin Isbarn <bei@crosscan.com>
2024-02-19 20:12:57 +0100

    Add an abstraction to keep it dry

--- memory-serve-macros/src/lib.rs
+++ memory-serve-macros/src/lib.rs
@@ -7,25 +7,8 @@ mod utils;
 
 use crate::asset::Asset;
 
-#[proc_macro]
-pub fn load_assets(input: TokenStream) -> TokenStream {
-    let input = input.to_string();
-    let input = input.trim_matches('"');
-    let mut asset_path = Path::new(&input).to_path_buf();
-
-    // skip if a subscriber is already registered (for instance by rust_analyzer)
-    let _ = tracing_subscriber::fmt()
-        .without_time()
-        .with_target(false)
-        .try_init();
-
-    if asset_path.is_relative() {
-        let crate_dir = env::var("CARGO_MANIFEST_DIR")
-            .expect("CARGO_MANIFEST_DIR environment variable not set");
-        asset_path = Path::new(&crate_dir).join(asset_path);
-    }
-
-    asset_path = asset_path
+fn load_assets_from_path(path: &Path) -> TokenStream {
+    let asset_path = path
         .canonicalize()
         .expect("Could not canonicalize the provided path");
 
@@ -58,6 +41,27 @@ pub fn load_assets(input: TokenStream) -> TokenStream {
 }
 
 #[proc_macro]
+pub fn load_assets(input: TokenStream) -> TokenStream {
+    let input = input.to_string();
+    let input = input.trim_matches('"');
+    let mut asset_path = Path::new(&input).to_path_buf();
+
+    // skip if a subscriber is already registered (for instance by rust_analyzer)
+    let _ = tracing_subscriber::fmt()
+        .without_time()
+        .with_target(false)
+        .try_init();
+
+    if asset_path.is_relative() {
+        let crate_dir = env::var("CARGO_MANIFEST_DIR")
+            .expect("CARGO_MANIFEST_DIR environment variable not set");
+        asset_path = Path::new(&crate_dir).join(asset_path);
+    }
+
+    load_assets_from_path(&asset_path)
+}
+
+#[proc_macro]
 pub fn load_assets_from_out_dir(input: TokenStream) -> TokenStream {
     let input = input.to_string();
     let input = input.trim_matches('"');
@@ -73,36 +77,8 @@ pub fn load_assets_from_out_dir(input: TokenStream) -> TokenStream {
         panic!("The asset path is supposed to be relative to the OUT_DIR");
     }
 
-    let mut asset_path = Path::new(&env::var("OUT_DIR").expect("OUT_DIR environment variable not set")).join(asset_path);
+    let asset_path = Path::new(&env::var("OUT_DIR").expect("OUT_DIR environment variable not set"))
+        .join(asset_path);
 
-    asset_path = asset_path
-        .canonicalize()
-        .expect("Could not canonicalize the provided path");
-
-    if !asset_path.exists() {
-        panic!("The path {:?} does not exists!", asset_path);
-    }
-
-    let files: Vec<Asset> = list_assets(&asset_path);
-
-    let route = files.iter().map(|a| &a.route);
-    let path = files.iter().map(|a| &a.path);
-    let content_type = files.iter().map(|a| &a.content_type);
-    let etag = files.iter().map(|a| &a.etag);
-    let bytes = files.iter().map(|a| &a.bytes);
-    let brotli_bytes = files.iter().map(|a| &a.brotli_bytes);
-
-    quote::quote! {
-        &[
-            #(memory_serve::Asset {
-                route: #route,
-                path: #path,
-                content_type: #content_type,
-                etag: #etag,
-                bytes: #bytes,
-                brotli_bytes: #brotli_bytes,
-            }),*
-        ]
-    }
-    .into()
+    load_assets_from_path(&asset_path)
 }
