12f9ee4652b231c507fe78924672418d29137e4f
Arjen <4867268+atezet@users.noreply.github.com>
2024-07-19 10:48:56 +0200

    Fix enum description

--- memory-serve/src/cache_control.rs
+++ memory-serve/src/cache_control.rs
@@ -4,10 +4,15 @@ use axum::http::{header::CACHE_CONTROL, HeaderName, HeaderValue};
 /// See [Cache control](index.html#cache-control)
 #[derive(Debug, Clone, Copy)]
 pub enum CacheControl {
+    /// clients can keep assets that have cache busting for a year: `"max-age=31536000, immutable"`
     Long,
+    /// assets without cache busting are revalidated after a day and can be kept for a week: `"max-age=604800, stale-while-revalidate=86400"`
     Medium,
+    /// cache kept for max 5 minutes, only at the client (not in a proxy): `"max-age:300, private"`
     Short,
+    /// do not cache if freshness is really vital: `"no-cache"`
     NoCache,
+    /// custom value
     Custom(&'static str),
 }
 
@@ -15,19 +20,15 @@ impl CacheControl {
     pub(crate) fn as_header(&self) -> (HeaderName, HeaderValue) {
         let value = match self {
             Self::Long => {
-                // clients can keep assets that have cache busting for a year
                 "max-age=31536000, immutable"
             }
             Self::Medium => {
-                // assets without cache busting are revalidated after a day and can be kept for a week
                 "max-age=604800, stale-while-revalidate=86400"
             }
             Self::Short => {
-                // cache kept for max 5 minutes, only at the client (not in a proxy)
                 "max-age:300, private"
             }
             Self::NoCache => {
-                // do not cache if freshness is really vital
                 "no-cache"
             }
             Self::Custom(value) => value,
