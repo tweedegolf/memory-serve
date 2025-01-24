59da8673418c722bc2010e49a876e8c16aa07f7b
Marlon Baeten <marlon@tweedegolf.com>
2025-01-24 10:59:36 +0100

    Escape special characters in the path

--- memory-serve-core/src/util.rs
+++ memory-serve-core/src/util.rs
@@ -2,6 +2,29 @@ use std::{io::Write, path::Path};
 
 use mime_guess::mime;
 
+const ALLOWED_CHARS: [(&str, &str); 20] = [
+    ("%2F", "/"),
+    ("%5C", "\\"),
+    ("%21", "!"),
+    ("%2A", "*"),
+    ("%27", "'"),
+    ("%28", "("),
+    ("%29", ")"),
+    ("%3B", ";"),
+    ("%3A", ":"),
+    ("%40", "@"),
+    ("%26", "&"),
+    ("%3D", "="),
+    ("%2B", "+"),
+    ("%24", "$"),
+    ("%2C", ","),
+    ("%2F", "/"),
+    ("%3F", "?"),
+    ("%25", "%"),
+    ("%5B", "["),
+    ("%5D", "]"),
+];
+
 /// Convert a path to a (HTTP) path / route
 pub(crate) fn path_to_route(base: &Path, path: &Path) -> String {
     let relative_path = path
@@ -16,6 +39,12 @@ pub(crate) fn path_to_route(base: &Path, path: &Path) -> String {
         })
         .collect::<Vec<&str>>()
         .join("/");
+
+    let mut route: String = urlencoding::encode(&route).to_string();
+
+    for (from, to) in ALLOWED_CHARS {
+        route = route.replace(from, to);
+    }
 
     format!("/{route}")
 }
@@ -38,4 +67,19 @@ pub(crate) fn compress_brotli(input: &[u8]) -> Option<Vec<u8>> {
     writer.write_all(input).ok()?;
 
     Some(writer.into_inner())
+}
+
+#[cfg(test)]
+mod test {
+    use crate::util::path_to_route;
+
+    #[test]
+    fn test_path_to_route() {
+        let base = std::path::Path::new("/");
+        let path = std::path::Path::new(
+            "/assets/stars:wow !@%^&*()ama{zi}ng💩! * ' ( ) ; : @ & = + $ , ? % [ ] \\.svg",
+        );
+
+        assert_eq!(path_to_route(base, path), "/assets/stars:wow%20!@%%5E&*()ama%7Bzi%7Dng%F0%9F%92%A9!%20*%20'%20(%20)%20;%20:%20@%20&%20=%20+%20$%20,%20?%20%%20[%20]%20\\.svg");
+    }
 }
